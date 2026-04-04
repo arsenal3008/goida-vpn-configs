4ab0ab9ddb1b47c74f3d03d79278204320b4672d
avencores <64781822+AvenCores@users.noreply.github.com>
2026-04-04 11:26:44 +0300

    update 23

--- source/main.py
+++ source/main.py
@@ -110,7 +110,7 @@ URLS = [
     "https://github.com/Argh94/Proxy-List/raw/refs/heads/main/All_Config.txt",                     # 20
     "https://raw.githubusercontent.com/shabane/kamaji/master/hub/merged.txt",                      # 21
     "https://raw.githubusercontent.com/wuqb2i4f/xray-config-toolkit/main/output/base64/mix-uri",  # 22
-    "https://raw.githubusercontent.com/WhitePrime/xraycheck/refs/heads/main/configs/available",   # 23
+    "https://github.com/igareck/vpn-configs-for-russia/raw/refs/heads/main/BLACK_VLESS_RUS.txt",   # 23
     "https://github.com/Mr-Meshky/vify/raw/refs/heads/main/configs/vless.txt",                     # 24
     "https://raw.githubusercontent.com/V2RayRoot/V2RayConfig/refs/heads/main/Config/vless.txt",   # 25
 ]
